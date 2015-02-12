274c7aebc2c6a3b409dcc6900efd97d34e829958
yaroslav.n <yaroslav.sib@yandex.ru>
2015-02-12 11:01:15 +0600

    Changed 1

--- 1.php
+++ 1.php
@@ -2,7 +2,7 @@
 
 $ids = array(1, 2, 3, 4);
 $ids_string = implode(", ", $ids);
-echo '$sql = \'SELECT\' * \'FROM \'table\' WHERE \'id\' IN'." ($ids_string)'";
+echo '$sql'." = 'SELECT' * 'FROM 'table' WHERE 'id' IN"." ($ids_string)'";
 
 
 
